071a9b1949d3570802ec8fcb68a25bc7d593d118
joyal shaji <jofi24228@gmail.com>
2024-06-23 16:49:07 +0530

    web app

---
+++ app.js
@@ -0,0 +1,60 @@
+function getBathValue() {
+    var uiBathrooms = document.getElementsByName("uiBathrooms");
+    for (var i in uiBathrooms) {
+        if (uiBathrooms[i].checked) {
+            return parseInt(i) + 1;
+        }
+    }
+    return -1; // Invalid Value
+}
+
+function getBHKValue() {
+    var uiBHK = document.getElementsByName("uiBHK");
+    for (var i in uiBHK) {
+        if (uiBHK[i].checked) {
+            return parseInt(i) + 1;
+        }
+    }
+    return -1; // Invalid Value
+}
+
+function onClickedEstimatePrice() {
+    console.log("Estimate price button clicked");
+    var sqft = document.getElementById("uiSqft").value;
+    var bhk = getBHKValue();
+    var bathrooms = getBathValue();
+    var location = document.getElementById("uiLocations").value;
+    var estPrice = document.getElementById("uiEstimatedPrice");
+
+    var url = "http://127.0.0.1:5000/predict_house_price"; // Assuming your Flask route for prediction
+
+    $.post(url, {
+        total_sqft: parseFloat(sqft),
+        bhk: bhk,
+        bath: bathrooms,
+        location: location
+    }, function (data, status) {
+        console.log(data);
+        estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+    });
+}
+
+function onPageLoad() {
+    console.log("document loaded");
+    var url = "http://127.0.0.1:5000/get_locations_names"; // Assuming your Flask route for fetching locations
+
+    $.post(url, function (data, status) {
+        console.log("got response for get_locations_names request");
+        if (data && data.locations) {
+            var locations = data.locations;
+            var uiLocations = document.getElementById("uiLocations");
+            $('#uiLocations').empty();
+            for (var i = 0; i < locations.length; i++) {
+                var opt = new Option(locations[i]);
+                uiLocations.appendChild(opt);
+            }
+        }
+    });
+}
+
+window.onload = onPageLoad;
